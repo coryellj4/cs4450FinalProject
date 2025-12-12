1d544bb3c0daf6f9669f24b148f7f20da3ecdcf0
coryellj4 <159492951+coryellj4@users.noreply.github.com>
2025-12-11 22:54:45 -0600

    makeTree filename
    Made makeTree not hardcoded

--- makeTree.py
+++ makeTree.py
@@ -2,16 +2,21 @@ from antlr4 import *
 from PythonParser3Lexer import PythonParser3Lexer
 from PythonParser3Parser import PythonParser3Parser
 from antlr4.tree.Trees import Trees
+import sys
 
+if len(sys.argv) < 2:
+    print("Usage: python maketree.py <filename>")
+    sys.exit(1)
 
-input_stream = FileStream("project_deliverable_3.py")
+filename = sys.argv[1]
+
+input_stream = FileStream(filename)
+
 
 lexer = PythonParser3Lexer(input_stream)
 stream = CommonTokenStream(lexer)
 parser = PythonParser3Parser(stream)
 
-
 tree = parser.program()
-
 
 print(Trees.toStringTree(tree, None, parser))
